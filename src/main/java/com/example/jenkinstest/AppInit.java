37c8be9c1547020c8a08b5b799a7064abf3a50cb
Hojung Yun <hojung_yun@yahoo.co.kr>
2018-06-11 14:53:31 +0900

    AppInit added

---
+++ src/main/java/com/example/jenkinstest/AppInit.java
@@ -0,0 +1,17 @@
+package com.example.jenkinstest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AppInit implements ApplicationRunner {
+
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("==============================");
+        log.info("App runs !!!!!!!!!!");
+        log.info("==============================");
+    }
+}
